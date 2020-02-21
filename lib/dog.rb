4ea28b51bb0002ae96e75d5c82c6622dc8a733bf
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-21 05:42:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,21 @@
 # Add your code here
 
 class Dog 
-  
+  attr_accessor :name
   @@all = []
   
-  def initialize
+  def initialize(name)
+    @name = name
     @@all << self
   end
   
   def all 
     @@all
+  end
+  
+  def print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
   end
 end
